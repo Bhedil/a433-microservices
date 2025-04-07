01dbf62c53979bc4be38e2a2db0830ea60a63a6b
Bhedil <aidilarifinnizar@gmail.com>
2025-04-07 16:35:18 +0700

    fix build script and add CircleCI config

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -2,7 +2,7 @@
 docker build -t bhedil/karsajobs-ui:latest .
 
 #Mengubah nama image sesuai dengan github packages dengan tag latest
-docker tag item-app:v1 ghcr.io/bhedil/bhedil/karsajobs-ui:latest
+docker tag bhedil/karsajobs-ui:latest ghcr.io/bhedil/bhedil/karsajobs-ui:latest
 
 #login ke github packages
 echo $CR_PAT | docker login ghcr.io -u Bhedil --password-stdin
